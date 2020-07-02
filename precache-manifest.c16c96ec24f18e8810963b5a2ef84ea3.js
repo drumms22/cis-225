a67aaa9263ccce7e90024e234ec16dcdaeb7ef61
drumms22 <62912267+drumms22@users.noreply.github.com>
2020-07-01 22:57:11 -0400

    Updates

---
+++ precache-manifest.c16c96ec24f18e8810963b5a2ef84ea3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc310deaf5c7b6c08d4e669616325a0a",
+    "url": "/cis-225/index.html"
+  },
+  {
+    "revision": "aba1ce961346bad62460",
+    "url": "/cis-225/static/css/main.2b1c688f.chunk.css"
+  },
+  {
+    "revision": "0277981caaec00fdc890",
+    "url": "/cis-225/static/js/2.6b17f422.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/cis-225/static/js/2.6b17f422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aba1ce961346bad62460",
+    "url": "/cis-225/static/js/main.10111e20.chunk.js"
+  },
+  {
+    "revision": "a72c07cb9e02fc0fb089",
+    "url": "/cis-225/static/js/runtime-main.ff1819cf.js"
+  }
+]);
